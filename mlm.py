79efbe2438baec24b45c74b45fc25b37272d8a35
Edward Ma <makcedward@gmail.com>
2021-06-04 23:12:57 -0700

    fix output_dir issue

--- mlm.py
+++ mlm.py
@@ -275,10 +275,10 @@ def main(input_args):
 
 		logger.info(f"epoch {epoch}: perplexity: {perplexity}")
 
-	if args.output_dir is not None:
+	if output_dir is not None:
 		accelerator.wait_for_everyone()
 		unwrapped_model = accelerator.unwrap_model(model)
-		unwrapped_model.save_pretrained(args.output_dir, save_function=accelerator.save)
+		unwrapped_model.save_pretrained(output_dir, save_function=accelerator.save)
 
 
 
